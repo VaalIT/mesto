ece4dec957ff748de251b78188d806adbc664d72
Valentina Lugovaia <vaal.it@yandex.ru>
2022-05-21 20:27:05 +0300

    restart 8 sprint

---
+++ scripts/Card.js
@@ -0,0 +1,56 @@
+import { openPopup, popupView, popupViewPhoto, popupViewTitle } from '../scripts/index.js'; 
+
+class Card { 
+  constructor(newPhoto, templateSelector) { 
+    this._name = newPhoto.name; 
+		this._link = newPhoto.link; 
+    this._templateSelector = templateSelector; 
+  } 
+
+  _getTemplate() { 
+    const cardItem = document 
+      .querySelector(this._templateSelector) 
+      .content 
+      .querySelector('.photo__item') 
+      .cloneNode(true); 
+
+    this._element = cardItem; 
+  } 
+
+  _setEventListeners() { 
+		this._element.querySelector('.photo__delete-button').addEventListener('click', () => { 
+			this._element.remove(); 
+      this._element = null; 
+		}); 
+
+    this._element.querySelector('.photo__image').addEventListener('click', () => { 
+			this._cardItemClick(); 
+		});
+
+    this._element.querySelector('.photo__like-button').addEventListener('click', () => { 
+			this._element.querySelector('.photo__like-button').classList.toggle('photo__like-button_active'); 
+		});
+	}   
+
+  _cardItemClick() { 
+    popupViewTitle.textContent = this._name; 
+    popupViewPhoto.alt = 'Фото ' + this._name + '.'; 
+    popupViewPhoto.src = this._link; 
+    openPopup(popupView);
+  }
+
+  generateCard() { 
+    this._getTemplate(); 
+    this._setEventListeners();
+
+    const photoImage = this._element.querySelector('.photo__image');
+
+    photoImage.src = this._link; 
+	  photoImage.alt = 'Фото ' + this._name + '.'; 
+	  this._element.querySelector('.photo__title').textContent = this._name; 
+
+    return this._element; 
+  } 
+} 
+
+export default Card; 
--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -4,20 +4,20 @@ class FormValidator {
     this._submitButtonSelector = enableValidation.submitButtonSelector;
     this._inactiveButtonClass = enableValidation.inactiveButtonClass;
     this._inputErrorClass = enableValidation.inputErrorClass;
-    this._errorClass = enableValidation.errorClass;
+    this._errorClass = enableValidation.errorClass; 
 
     this._formItem = formItem;
 
     this._inputList =  Array.from(this._formItem.querySelectorAll(this._inputSelector));
     this._buttonItem = this._formItem.querySelector(this._submitButtonSelector);
-  }
+  } 
 
   _showError = (input, errorMessageText) => {
     const _errorElement = this._formItem.querySelector(`#${input.id}-error`);
     _errorElement.classList.add(this._errorClass);
     _errorElement.textContent = errorMessageText;
     input.classList.add(this._inputErrorClass);
-  }
+  } 
 
   _hideError = (input) => {
     const _errorElement = this._formItem.querySelector(`#${input.id}-error`);
@@ -44,19 +44,18 @@ class FormValidator {
         this.toggleButtonState();
       });
     });
-  }
+  } 
 
   enableValidation = () => {
     this._formItem.addEventListener('submit', function (evt) {
       evt.preventDefault();
     });
-    
     this._setEventListeners();
   }
 
   _hasInvalidInput = () => {
     return this._inputList.some((input) => !input.validity.valid);
-  }
+  } 
 
   toggleButtonState = () => {
     if (this._hasInvalidInput(this._inputList)) {
@@ -65,9 +64,8 @@ class FormValidator {
     } else {
       this._buttonItem.classList.remove(this._inactiveButtonClass);
       this._buttonItem.disabled = false;
-    } 
+    }
   }
-}
+} 
 
-export default FormValidator;
-
+export default FormValidator; 
